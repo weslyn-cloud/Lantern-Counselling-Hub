e3279a01adc429b297b4533b6052f80224940177
weslyn-cloud <weslyn@lanterncounselling.com.au>
2026-03-24 15:16:09 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Lantern-Counselling-Hub/', // Fixes the blank page issue
+      base: '/Lantern-Counselling-Hub/',
       server: {
         port: 3000,
         host: '0.0.0.0',
